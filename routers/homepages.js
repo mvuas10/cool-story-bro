ec28642e03e5bdbc3c4ee21688864a3d05631173
mvuas10 <mvuas10@gmail.com>
2020-05-31 15:11:22 +0200

    The frontend route `/homepages/:id` displays a detail page for a homepage

--- routers/homepages.js
+++ routers/homepages.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const HomePage = require("../models").homepage;
+const Story = require("../models").story;
 
 const router = new Router();
 
@@ -7,6 +8,22 @@ router.get("/", async (req, res, next) => {
   try {
     const allHomepages = await HomePage.findAll();
     res.status(200).json(allHomepages);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    const homepage = await HomePage.findByPk(id, { include: [Story] });
+
+    if (!homepage) {
+      res.status(404).send("Homepage not found");
+    } else {
+      res.status(200).json(homepage);
+    }
   } catch (error) {
     next(error);
   }
